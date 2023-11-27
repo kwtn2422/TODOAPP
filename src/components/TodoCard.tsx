3d61bed748242edc8003f5f504cbceb4ae86b0fb
kwtn2422 <kwtn2422@gmail.com>
2023-11-27 15:25:22 +0900

    fearure/status chnage

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -9,16 +9,28 @@ export type TodoCardProps = {
 };
 
 const statusClass = {
-  0: { bgColor: "bg-green-200", statement: "完了" },
-  1: { bgColor: "bg-yellow-200", statement: "進行中" },
+  0: { bgColor: "bg-yellow-200", statement: "進行中" },
+  1: { bgColor: "bg-green-200", statement: "完了" },
   2: { bgColor: "bg-gray-200", statement: "未実施" },
 };
 
-const TodoCard: React.FC<TodoCardProps> = ({ title, description, status }) => {
+const TodoCard: React.FC<TodoCardProps> = ({
+  title,
+  description,
+  status: initialStatus,
+}) => {
+  const [status, setStatus] = useState<statusType>(initialStatus);
+
+  const handleStatusChange = () => {
+    const nextStatus: statusType = ((status + 1) % 3) as statusType;
+    setStatus(nextStatus);
+  };
+
   return (
     <div className="flex shadow-md rounded w-full mb-1">
       <button
         className={`basis-1/6 h-20 flex items-center rounded-l ${statusClass[status].bgColor}`}
+        onClick={handleStatusChange}
       >
         {statusClass[status].statement}
       </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Form from "@/components/Form";
-import { TodoCardProps, statusType } from "@/components/TodoCard";
+import { TodoCardProps } from "@/components/TodoCard";
 import TodoList from "@/components/TodoList";
 import React, { useState } from "react";
 
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <div className="flex justify-center">
       <div className="flex flex-col w-80 items-center">
-        <p className=" text-3xl m-2">Todo Apps</p>
+        <p className=" text-3xl m-2">Todo List</p>
         <Form onClick={addList}></Form>
         <TodoList todoListArray={list}></TodoList>
       </div>
