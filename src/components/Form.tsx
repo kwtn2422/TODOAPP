46d4a6f3e2ea7fd31de35630ae940d33cfcbca32
kwtn2422 <kwtn2422@gmail.com>
2023-11-28 14:20:37 +0900

    titleかdescriptionが空の時はエラーを返すように修正

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -11,9 +11,13 @@ const Form: React.FC<FormProps> = ({ onClick }) => {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    onClick({ title: title, description: description, status: 2 });
-    setTitle("");
-    setDiscription("");
+    if (title.trim() && description.trim()) {
+      onClick({ title: title, description: description, status: 2 });
+      setTitle("");
+      setDiscription("");
+    } else {
+      alert("titleかdescriptionが空です");
+    }
   };
 
   return (
